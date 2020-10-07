dcd7d61e6430a98ba287b265bc1edbfb3b7ee73f
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2020-10-07 11:16:06 -0300

    arquivos
    upload dos arquivos do projeto

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LoL - Champions Data
+***
  Este é um projeto sem nenhum fim lucrativo, o único intuito para sua realização foi para estudar mais como manipular API's com Javascript
+
+ [Api utilizada](https://api-lol.herokuapp.com)
---
+++ js/script.js
@@ -0,0 +1,50 @@
+let xhttp = new XMLHttpRequest()
+xhttp.responseType = 'json'
+xhttp.onload = function(){
+    try{
+        useResponse(this.response)
+    }
+    catch{
+        window.alert('Type a valid name')
+        document.getElementById('github').style.position = 'absolute' //volta o footer ao lugar correto quando o conteudo eh apagado
+    }
+}
+
+function useResponse(resp){ //Esta funcao eh responsavel por receber o json e utiliza-lo no html
+    let dataContainer = document.getElementById('data-container')
+    dataContainer.innerHTML = ''    //limpa os dados do campeao anterior
+
+    let nameAndAlias = document.createElement('h2')
+    let ChampionInformation = document.createElement('p')
+
+    let imageChampion = document.createElement('img')
+    imageChampion.setAttribute('src', resp["skins"][0])
+    imageChampion.setAttribute('id', 'ChampionImage')
+
+    for(let i = 0; i < resp["abilities"].length; i++){  //passagem dos dados do json
+        ChampionInformation.innerHTML += `<strong>${resp["abilities"][i]["type"]}</strong>: ${resp["abilities"][i]["name"]}<br>`
+        ChampionInformation.innerHTML += `<strong>Ability Description: </strong> ${resp["abilities"][i]["description"]} <br><br>`
+    }
+
+    nameAndAlias.innerHTML = `${resp["name"]} - ${resp["alias"]}`
+    nameAndAlias.style.textTransform = 'uppercase'
+    nameAndAlias.style.textAlign = 'center'
+    nameAndAlias.style.marginTop = '20px'
+
+    ChampionInformation.style.marginTop = '30px'
+    ChampionInformation.style.borderTop = '2px solid black'
+    ChampionInformation.style.paddingTop = '20px'
+    ChampionInformation.style.textAlign = 'justify'
+
+    dataContainer.appendChild(nameAndAlias)
+    dataContainer.appendChild(imageChampion)
+    dataContainer.appendChild(ChampionInformation)
+
+    document.getElementById('github').style.position = 'relative' //isso faz com que o footer continue no fim da pagina
+}
+
+function searchName(){
+    let serch = document.getElementById('c-name').value
+    xhttp.open('GET', `https://api-lol.herokuapp.com/api/champions/${serch}`)
+    xhttp.send()
+}
